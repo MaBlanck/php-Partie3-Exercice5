f1c2617db4d4855011231c02d0740932185a3a22
Max <maxblanck@outlook.fr>
2020-12-11 11:46:42 +0100

    php Partie 3  Exercice 5

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Exercice 5 partie 3</title>
 </head>
+
 <body>
     <h1>Exercice 5 Partie 3</h1>
 
-    <ul>
-    <?php 
-    for($number = 1; $number <= 15; $number++)
-    {
-        echo '<li>' . 'On y arrive presque' . '</li>';
-    }
-    ?>
-    </ul>
+        <?php
+        for ($number = 1; $number <= 15; $number++): ?>
+            <p>On y arrive presque</p>
+        <?php endfor; ?>
 </body>
+
 </html>
